bc2c2eb2bade115b190be87e9cd2d448664dafbe
ajamoranto <30665360+ajamoranto@users.noreply.github.com>
2017-08-17 08:43:25 -0700

    Initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+(function(){
+
+  //Team One
+  let oneShoot = document.querySelector("#teamone-shoot");
+  let oneShots = document.querySelector("#teamone-numshots");
+  let oneGoals = document.querySelector("#teamone-numhits");
+  //Team Two
+  let twoShoot = document.querySelector("#teamtwo-shoot");
+  let twoShots = document.querySelector("#teamtwo-numshots");
+  let twoGoals = document.querySelector("#teamtwo-numhits");
+  //Reset
+  let reset = document.querySelector("#reset");
+  let resetCounter = document.querySelector("#num-resets");
+  //Sound
+  let shotSound = document.querySelector("#shotSound");
+  let scoreSound = document.querySelector("#scoreSound");
+  let buzzer = document.querySelector("#buzzer");
+
+
+  //Team One function
+  oneShoot.addEventListener("click", function(){
+
+    shotSound.play();
+    oneShots.innerHTML = parseInt(oneShots.innerHTML) + 1
+
+    shot = (Math.floor(Math.random() * 2) == 0);
+    if (shot){
+      oneGoals.innerHTML = parseInt(oneGoals.innerHTML) + 1;
+      scoreSound.play();
+    }
+
+  })
+
+  //Team Two function
+  twoShoot.addEventListener("click", function(){
+
+    shotSound.play();
+    twoShots.innerHTML = parseInt(twoShots.innerHTML) + 1
+    shot = (Math.floor(Math.random() * 2) == 0);
+    if (shot){
+      twoGoals.innerHTML = parseInt(twoGoals.innerHTML) + 1;
+      scoreSound.play();
+    }
+
+  })
+
+  //Reset function
+  reset.addEventListener("click", function(){
+
+    buzzer.play();
+
+    if (oneGoals.innerHTML > twoGoals.innerHTML) {
+      alert("The Fighting Mongooses Win!")
+    } else if (twoGoals.innerHTML > oneGoals.innerHTML) {
+      alert("Coney Island Whitefish Wins!")
+    } else {
+      alert("No one likes a draw...")
+    }
+
+    oneShots.innerHTML = 0;
+    oneGoals.innerHTML = 0;
+    twoShots.innerHTML = 0;
+    twoGoals.innerHTML = 0;
+    resetCounter.innerHTML = parseInt(resetCounter.innerHTML) + 1;
+
+  })
+})();
